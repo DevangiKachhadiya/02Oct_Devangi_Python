daa2b1150602187af169eb52cf622025f39a9a62
DevangiKachhadiya <devangikachhadiya37@gmail.com>
2024-10-18 22:15:06 +0530

    addstr

--- Assignment/Module – 2/str.py
+++ Assignment/Module – 2/str.py
@@ -9,16 +9,9 @@ new_s1=s2[:2]+s1[2:]
 new_s2=s1[:2]+s2[2:]
 print(new_s1 + "  " + new_s2)
 new_str=(s1,s2)
-#res=swap(s1,s2)
-'''def swap_first_two_chars(str1,str2):
-    print(str1,str2)
 
-    new_str1 = str2[:2] + str1[2:]
-    new_str2 = str1[:2] + str2[2:]
-    return new_str1 +" " + new_str2
 
-string1="abcd"
-string2="kbn"
-result = swap_first_two_chars(string1,string2)
-print(result)
-'''
+
+
+
+
